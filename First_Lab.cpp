f19c0b0ce0b6b38ff5c488d5800b95e3ae2e55f7
IIo3u <bylov4@yandex.ru>
2023-10-11 18:05:46 +0300

    Поменял Список на доработанный

--- First_Lab.cpp
+++ First_Lab.cpp
@@ -1,11 +1,10 @@
 
-#include <iostream>		// ({[]})
-// ([]}{)
+#include <iostream>
 
 using namespace std;
 
 template<typename SomeType>
-struct List
+class List
 {
 private:
 	template<typename SomeType>
@@ -23,201 +22,36 @@ private:
 		}
 	};
 
-	void add_first(SomeType data);
 
 	Node<SomeType>* Head;
 	Node<SomeType>* Tail;
 	int Size;
+	void add_first(SomeType data);
 
 public:
+
 	List();
-	void pop_element(int index);
-	void pop_element(Node<SomeType>* data);
-	void pop_low();
-	void pop_top();
+
+	void add_on_pos_right(int index, SomeType data);
+	void add_on_pos_left(int index, SomeType data);
 	void append(SomeType data);
 	void add_front(SomeType data);
+	void pop_element(int index);
+	void pop_top();
+	void pop_low();
 	SomeType& get_low();
 	SomeType& get_top();
 	int get_size() { return Size; };
 	SomeType& operator[](int index);
-	List<SomeType>& Copy(List<SomeType> List);
+
 
 };
-
-template<typename SomeType>
-List<SomeType>::List()
-{
-	Size = 0;
-	Head = Tail = nullptr;
-
-};
-
 template<typename SomeType>
 void List<SomeType>::add_first(SomeType data)
 {
 	Tail = new Node<SomeType>(data);
 	Head = Tail;
-}
-
-template<typename SomeType>
-void List<SomeType>::append(SomeType data)
-{
-	if (data != NULL)
-	{
-		if (this->Tail == nullptr)
-		{
-			this->add_first(data);
-		}
-		else
-		{
-			Node<SomeType>* Previous;
-			Node<SomeType>* Current = this->Tail;
-			Current->NextAddress = new Node<SomeType>(data);
-			Previous = Current;
-			Current = Current->NextAddress;
-			Current->PreviousAddress = Previous;
-			this->Tail = Current;
-		}
-		Size++;
-	}
-	else
-	{
-		cout << "Enter a valid data!" << endl;
-	}
-}
-
-template<typename SomeType>
-void List<SomeType>::add_front(SomeType data)
-{
-	if (data != NULL)
-	{
-		if (Head == nullptr)
-		{
-			this->add_first(data);
-		}
-		else
-		{
-			Node<SomeType>* Next;
-			Node<SomeType>* Current = this->Head;
-			Current->PreviousAddress = new Node<SomeType>(data);
-			Next = Current;
-			Current = Current->PreviousAddress;
-			Current->NextAddress = Next;
-			this->Head = Current;
-		}
-		Size++;
-	}
-	else
-	{
-		cout << "Enter a valid data!" << endl;
-	}
-}
-
-template<typename SomeType>
-SomeType& List<SomeType>::get_low()
-{
-	return this->Tail->data;
-};
-
-template<typename SomeType>
-SomeType& List<SomeType>::get_top()
-{
-	return this->Head->data;
-};
-
-template<typename SomeType>
-void List<SomeType>::pop_element(int index)
-{
-	if (this->get_size() == 0)
-	{
-		cout << "This list has no elements!\n";
-		return;
-	}
-	if (this->get_size() == 1)
-	{
-		delete this->Head;
-		this->Size--;
-		return;
-	}
-
-	int counter = 0;
-	Node<SomeType>* Previous = nullptr;
-	Node<SomeType>* Current = Head;
-	Node<SomeType>* Next = nullptr;
-	while (counter != index)
-	{
-		Current = Current->NextAddress;
-		counter++;
-	}
-	Previous = Current->PreviousAddress;
-	Next = Current->NextAddress;
-	if (Previous != nullptr)
-	{
-		Previous->NextAddress = Next;
-	}
-
-	if (Next != nullptr)
-	{
-		Next->PreviousAddress = Previous;
-	}
-	delete Current;
-	Size--;
-}
-
-template<typename SomeType>
-void List<SomeType>::pop_element(Node<SomeType>* Data)
-{
-	if (this->get_size() == 0)
-	{
-		cout << "This list has no elements!\n";
-		return;
-
-		if (this->get_size() == 1)
-		{
-			delete this->Head;
-			this->Size--;
-			return;
-		}
-	}
-	Node<SomeType>* Previous = nullptr;
-	Node<SomeType>* Current = Data;
-	Node<SomeType>* Next = nullptr;
-	Previous = Current->PreviousAddress;
-	Next = Current->NextAddress;
-	if (Previous != nullptr)
-	{
-		Previous->NextAddress = Next;
-	}
-
-	if (Next != nullptr)
-	{
-		Next->PreviousAddress = Previous;
-	};
-	delete Current;
-	Size--;
-}
-
-template<typename SomeType>
-void List<SomeType>::pop_top()
-{
-	if (this->get_size() == 0)
-	{
-		cout << "This list has no elements!\n";
-		return;
-	}
-	if (this->get_size() == 1)
-	{
-		delete this->Head;
-		this->Size--;
-		return;
-	}
-	Node<SomeType>* Current = Head;
-	Current = Current->NextAddress;
-	this->Head = Current;
-	delete Current->PreviousAddress;
-	Current->PreviousAddress = nullptr;
-	this->Size--;
+	this->Size++;
 };
 
 template<typename SomeType>
@@ -244,6 +78,253 @@ void List<SomeType>::pop_low()
 };
 
 template<typename SomeType>
+void List<SomeType>::pop_top()
+{
+	if (this->get_size() == 0)
+	{
+		cout << "This list has no elements!\n";
+		return;
+	}
+	if (this->get_size() == 1)
+	{
+		delete this->Head;
+		this->Size--;
+		return;
+	}
+	Node<SomeType>* Current = Head;
+	Current = Current->NextAddress;
+	this->Head = Current;
+	delete Current->PreviousAddress;
+	Current->PreviousAddress = nullptr;
+	this->Size--;
+};
+
+template<typename SomeType>
+List<SomeType>::List()
+{
+	Size = 0;
+	Head = Tail = nullptr;
+
+};
+
+template<typename SomeType>
+void List<SomeType>::add_on_pos_right(int index, SomeType data)
+{
+	if (this->get_size() == 0)
+	{
+		this->add_first(data);
+	}
+	else
+	{
+		if (index == this->Size - 1)
+		{
+			this->append(data);
+		}
+		else
+		{
+			int counter = 0;
+			Node<SomeType>* NewNode = new Node<SomeType>(data);
+			Node<SomeType>* Current = this->Head;
+			while (index != counter)
+			{
+				Current = Current->NextAddress;
+				counter++;
+			}
+			Node<SomeType>* Previous = Current;
+			Node<SomeType>* Next = Current->NextAddress;
+			Current = NewNode;
+			Previous->NextAddress = Current;
+			Current->NextAddress = Next;
+			Next->PreviousAddress = Current;
+			Current->PreviousAddress = Previous;
+			this->Size++;
+		}
+	}
+}
+
+template<typename SomeType>
+void List<SomeType>::add_on_pos_left(int index, SomeType data)
+{
+	if (this->get_size() == 0)
+	{
+		this->add_first(data);
+	}
+	else
+	{
+		if (index == 0)
+		{
+			this->add_front(data);
+		}
+		else
+		{
+			int counter = 0;
+			Node<SomeType>* NewNode = new Node<SomeType>(data);
+			Node<SomeType>* Current = this->Head;
+			while (index != counter)
+			{
+				Current = Current->NextAddress;
+				counter++;
+			}
+			Node<SomeType>* Next = Current;
+			Node<SomeType>* Previous = Current->PreviousAddress;
+			Current = NewNode;
+			Next->PreviousAddress = Current;
+			Current->PreviousAddress = Previous;
+			Previous->NextAddress = Current;
+			Current->NextAddress = Next;
+			this->Size++;
+		}
+	}
+}
+
+template<typename SomeType>
+void List<SomeType>::append(SomeType data)
+{
+	if (this->Tail == nullptr)
+	{
+		this->add_first(data);
+		return;
+	}
+	else
+	{
+		this->Tail->NextAddress = new Node<SomeType>(data);
+		Node<SomeType>* Buffer = this->Tail;
+		this->Tail = this->Tail->NextAddress;
+		this->Tail->PreviousAddress = Buffer;
+		Size++;
+	}
+
+}
+
+template<typename SomeType>
+void List<SomeType>::add_front(SomeType data)
+{
+	if (Head == nullptr)
+	{
+		this->add_first(data);
+		return;
+	}
+	else
+	{
+		Node<SomeType>* Next;
+		Node<SomeType>* Current = this->Head;
+		Current->PreviousAddress = new Node<SomeType>(data);
+		Next = Current;
+		Current = Current->PreviousAddress;
+		Current->NextAddress = Next;
+		this->Head = Current;
+		Size++;
+	}
+
+}
+
+template<typename SomeType>
+void List<SomeType>::pop_element(int index)
+{
+
+	if (this->get_size() == 0)
+	{
+		return;
+	}
+	if (this->get_size() == 1)
+	{
+		delete this->Head;
+		this->Size--;
+		return;
+	}
+	if (index == this->Size - 1)
+	{
+		Node<SomeType>* Current = Tail;
+		Current = Current->PreviousAddress;
+		this->Tail = Current;
+		delete Current->NextAddress;
+		Current->NextAddress = nullptr;
+		this->Size--;
+		return;
+	}
+	if (index == 0)
+	{
+		Node<SomeType>* Current = Head;
+		Current = Current->NextAddress;
+		this->Head = Current;
+		delete Current->PreviousAddress;
+		Current->PreviousAddress = nullptr;
+		this->Size--;
+		return;
+	}
+	else
+	{
+		int counter = 0;
+		Node<SomeType>* Previous = nullptr;
+		Node<SomeType>* Current = Head;
+		Node<SomeType>* Next = nullptr;
+		while (counter != index)
+		{
+			Current = Current->NextAddress;
+			counter++;
+		}
+		Previous = Current->PreviousAddress;
+		Next = Current->NextAddress;
+		if (Previous != nullptr)
+		{
+			Previous->NextAddress = Next;
+		}
+
+		if (Next != nullptr)
+		{
+			Next->PreviousAddress = Previous;
+		}
+		delete Current;
+		this->Size--;
+	}
+
+}
+
+template<typename SomeType>
+SomeType& List<SomeType>::get_low()
+{
+	return this->Tail->data;
+};
+
+template<typename SomeType>
+SomeType& List<SomeType>::get_top()
+{
+	return this->Head->data;
+};
+
+template<typename SomeType>
+SomeType& List<SomeType>::operator[](int index)
+{
+	if (index == 0)
+	{
+		return this->Head->data;
+
+	}
+	else
+	{
+		if (index == this->Size - 1)
+		{
+			return this->Tail->data;
+		}
+		else
+		{
+			int counter = 0;
+			Node<SomeType>* Current = this->Head;
+			while (Current != nullptr)
+			{
+				if (counter == index)
+				{
+					return Current->data;
+				}
+				Current = Current->NextAddress;
+				counter++;
+			}
+		}
+
+	}
+};
+
+template<typename SomeType>
 List<SomeType>& Copy(List<SomeType>* OldList)
 {
 	List<SomeType>* NewList = new List<SomeType>;
@@ -253,21 +334,6 @@ List<SomeType>& Copy(List<SomeType>* OldList)
 	}
 }
 
-template<typename SomeType>
-SomeType& List<SomeType>::operator[](int index)
-{
-	int counter = 0;
-	Node<SomeType>* Current = this->Head;
-	while (Current != nullptr)
-	{
-		if (counter == index)
-		{
-			return Current->data;
-		}
-		Current = Current->NextAddress;
-		counter++;
-	}
-};
 
 template<typename SomeType>
 bool DefineSequence(List<SomeType> Sequence)
@@ -330,11 +396,11 @@ int main()
 
 	if (DefineSequence(firstStroke) == true)
 	{
-		cout << "This stroke is a valid algebraic expression" << endl;
+		cout << "True expression" << endl;
 	}
 	else
 	{
-		cout << "This stroke isn't a valid algebraic expression" << endl;
+		cout << "False expression" << endl;
 	}
 
 
